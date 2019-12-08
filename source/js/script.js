c0c2db47e4400abc28644eb9294614e4e74afa50
Maxim Safronov <1maximsafronov@gmail.com>
2019-12-08 15:22:48 +0700

    Улучшил скрипт для главного меню и сделал его резиновым

--- source/js/script.js
+++ source/js/script.js
@@ -1,27 +1,14 @@
 // Скрипты
 var menu = document.querySelector('.main-nav__site-nav');
 var menuToggle = document.querySelector('.main-nav__toggle');
-
+//
 if(menu && menuToggle) {
-  mobileMenu();
-  document.addEventListener("DOMContentLoaded", function(event){
-      window.onresize = function() {
-        mobileMenu();
-      };
-  });
-}
+  menu.classList.remove('main-nav__site-nav--no-js');
+  menuToggle.classList.remove('main-nav__toggle--no-js');
 
-function mobileMenu() {
-  if(window.matchMedia('(max-width: 767px)').matches) {
-    menu.classList.add("main-nav__site-nav--close");
-    menuToggle.classList.add("main-nav__toggle--close");
-    menuToggle.addEventListener("click", function(event) {
-      event.preventDefault();
-      menu.classList.toggle('main-nav__site-nav--close');
-      menuToggle.classList.toggle('main-nav__toggle--close');
-    });
-  } else {
-    menu.classList.remove("main-nav__site-nav--close");
-    menuToggle.classList.remove("main-nav__toggle--close");
-  }
+  menuToggle.addEventListener("click", function(event) {
+    event.preventDefault();
+    menu.classList.toggle('main-nav__site-nav--open');
+    menuToggle.classList.toggle('main-nav__toggle--open');
+  });
 }
