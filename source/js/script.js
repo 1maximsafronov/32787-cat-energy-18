95c66449e41eedd95e94052bae84e995d497e951
Maxim Safronov <1maximsafronov@gmail.com>
2019-12-13 07:56:10 +0700

    Подогнал мобильную и планшетную версии блока "До После"

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,63 @@ if(menu && menuToggle) {
     menuToggle.classList.toggle('main-nav__toggle--open');
   });
 }
+
+// // слайдер До После
+// var controls = document.querySelector('.controls');
+// var controlsToggle = controls.querySelector('.controls__toggle');
+// var controlsRange = document.querySelector('.controls__range');
+// var pictureBefore = document.querySelector('.example__picture--before');
+// var pictureAfter = document.querySelector('.example__picture--after');
+// var startPointMin;
+// var startPointMax;
+// pictureBefore.style.width = 354 + 'px';
+// pictureAfter.style.width = 354 + 'px';
+// controlsToggle.style.left = 197
+//
+//
+// controlsToggle.addEventListener('mousedown', function(evnt) {
+//   evnt.preventDefault();
+//   startPointMin = evnt.clientX;
+//   function onMouseMove(moveEvnt) {
+//     moveEvnt.preventDefault();
+//     var newPoint = startPointMin - moveEvnt.clientX;
+//     startPointMin = moveEvnt.clientX;
+//     if ((controlsToggle.offsetLeft - newPoint) >= 0 && (controlsToggle.offsetLeft - newPoint) <= (428 - 34)) {
+//       controlsToggle.style.left = (controlsToggle.offsetLeft - newPoint) + 'px';
+//       pictureBefore.style.width = (140 + 18 + controlsToggle.offsetLeft - newPoint) + 'px';
+//       pictureAfter.style.width = (708 - 140 - 16ы - controlsToggle.offsetLeft - newPoint) + 'px';
+//     };
+//   };
+//
+//   function onMouseUp(upEvnt) {
+//     upEvnt.preventDefault();
+//     document.removeEventListener('mousemove', onMouseMove);
+//     document.removeEventListener('mouseup', onMouseUp);
+//   };
+//   document.addEventListener('mousemove', onMouseMove);
+//   document.addEventListener('mouseup', onMouseUp);
+// });
+
+// для тачскрина
+// controlsToggle.addEventListener('touchstart', function(evnt) {
+//   evnt.preventDefault();
+//   startPointMin = evnt.pageX;
+//   function onTouchMove(moveEvnt) {
+//     moveEvnt.preventDefault();
+//     var newPoint = startPointMin - moveEvnt.pageX;
+//     startPointMin = moveEvnt.pageX;
+//     if ((controlsToggle.offsetLeft - newPoint) >= 0 && (controlsToggle.offsetLeft - newPoint) <= (428 - 34)) {
+//       controlsToggle.style.left = (controlsToggle.offsetLeft - newPoint) + 'px';
+//       // pictureBefore.style.width = (controlsToggle.offsetLeft - newPoint) + 'px';
+//       // pictureAfter.style.width = (400 - controlsToggle.offsetLeft - newPoint) + 'px';
+//     };
+//   };
+//
+//   function onTouchEnd(upEvnt) {
+//     upEvnt.preventDefault();
+//     document.removeEventListener('touchmove', onTouchMove);
+//     document.removeEventListener('touchend', onTouchEnd);
+//   };
+//   document.addEventListener('touchmove', onTouchMove);
+//   document.addEventListener('touchend', onTouchEnd);
+// });
