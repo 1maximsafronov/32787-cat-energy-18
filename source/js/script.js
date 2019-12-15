98c832ba02585e6bdcf506b86842702315714159
Maxim Safronov <1maximsafronov@gmail.com>
2019-12-15 11:25:12 +0700

    Правки по замечаниям

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,3 @@
-// Скрипты
 var menu = document.querySelector('.main-nav__site-nav');
 var menuToggle = document.querySelector('.main-nav__toggle');
 //
@@ -6,8 +5,8 @@ if(menu && menuToggle) {
   menu.classList.remove('main-nav__site-nav--no-js');
   menuToggle.classList.remove('main-nav__toggle--no-js');
 
-  menuToggle.addEventListener("click", function(event) {
-    event.preventDefault();
+  menuToggle.addEventListener("click", function(evt) {
+    evt.preventDefault();
     menu.classList.toggle('main-nav__site-nav--open');
     menuToggle.classList.toggle('main-nav__toggle--open');
   });
@@ -19,20 +18,18 @@ if(menu && menuToggle) {
 // var controlsRange = document.querySelector('.controls__range');
 // var pictureBefore = document.querySelector('.example__picture--before');
 // var pictureAfter = document.querySelector('.example__picture--after');
-// var startPointMin;
-// var startPointMax;
+// var startPoint;
 // pictureBefore.style.width = 354 + 'px';
 // pictureAfter.style.width = 354 + 'px';
 // controlsToggle.style.left = 197
 //
-//
-// controlsToggle.addEventListener('mousedown', function(evnt) {
-//   evnt.preventDefault();
-//   startPointMin = evnt.clientX;
+// controlsToggle.addEventListener('mousedown', function(evt) {
+//   evt.preventDefault();
+//   startPoint = evt.clientX;
 //   function onMouseMove(moveEvnt) {
 //     moveEvnt.preventDefault();
-//     var newPoint = startPointMin - moveEvnt.clientX;
-//     startPointMin = moveEvnt.clientX;
+//     var newPoint = startPoint - moveEvnt.clientX;
+//     startPoint = moveEvnt.clientX;
 //     if ((controlsToggle.offsetLeft - newPoint) >= 0 && (controlsToggle.offsetLeft - newPoint) <= (428 - 34)) {
 //       controlsToggle.style.left = (controlsToggle.offsetLeft - newPoint) + 'px';
 //       pictureBefore.style.width = (140 + 18 + controlsToggle.offsetLeft - newPoint) + 'px';
